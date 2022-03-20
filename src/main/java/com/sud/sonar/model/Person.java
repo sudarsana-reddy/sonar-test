ee14be6ed9d6fe83f076b8a40160cb7d80312a30
Sudarsana Ala <sudarsana.ala@gmail.com>
2022-03-20 18:24:04 +0530

    Added model and NullArgument Classes

---
+++ src/main/java/com/sud/sonar/model/Person.java
@@ -0,0 +1,15 @@
+package com.sud.sonar.model;
+
+public class Person {
+    String name;
+    String age;
+
+    public Person(String name, String age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Boolean nameMatches(String name){
+        return this.name.equalsIgnoreCase(name);
+    }
+}
