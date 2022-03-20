e21ef2a0e5aded72ba72c66a93c2c9d5a366986d
Sudarsana Ala <sudarsana.ala@gmail.com>
2022-03-20 16:42:39 +0530

    Added Substract method

--- src/main/java/com/sud/sonar/Calculator.java
+++ src/main/java/com/sud/sonar/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public Integer add(Integer a, Integer b){
         return a + b;
     }
+
+    public Integer sub(Integer a, Integer b){
+        return a - b;
+    }
 }
