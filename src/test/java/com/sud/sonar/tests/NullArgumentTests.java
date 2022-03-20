7d0aab976acb52683a98002433a74a14079ef72c
Sudarsana Ala <sudarsana.ala@gmail.com>
2022-03-20 18:46:31 +0530

    Added @Test tag to the test

--- src/test/java/com/sud/sonar/tests/NullArgumentTests.java
+++ src/test/java/com/sud/sonar/tests/NullArgumentTests.java
@@ -2,9 +2,11 @@ package com.sud.sonar.tests;
 
 import com.sud.sonar.model.Person;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class NullArgumentTests {
 
+    @Test
     public void personTest(){
         Person person = new Person("Sud", "23");
         boolean isTrue = person.nameMatches("sud");
